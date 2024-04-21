31bc37c0ebbdd09e48cba395b37aa45071ec4a2e
anitazaldivar <128723674+anitazaldivar@users.noreply.github.com>
2024-04-21 21:12:30 +0000

    done

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -1,16 +1,47 @@
 class ZebraController < ApplicationController
+  def rules
+    render({ :template => "game_templates/rules"})
+  end
+  
   def rock
     @random_move = ["rock", "paper", "scissors"].sample
+    if @random_move == "rock"
+      @outcome = "tied"
+  
+    elsif @random_move == "paper"
+      @outcome = "lost"
+  
+    elsif @random_move == "scissors"
+      @outcome = "won"
+    end
     render({ :template => "game_templates/play_rock"})
   end
 
   def paper
     @random_move = ["rock", "paper", "scissors"].sample
+    if @random_move == "paper"
+      @outcome = "tied"
+  
+    elsif @random_move == "scissors"
+      @outcome = "lost"
+  
+    elsif @random_move == "rock"
+      @outcome = "won"
+    end
     render({ :template => "game_templates/play_paper"})
   end
 
   def scissors
     @random_move = ["rock", "paper", "scissors"].sample
+    if @random_move == "scissors"
+      @outcome = "tied"
+  
+    elsif @random_move == "rock"
+      @outcome = "lost"
+  
+    elsif @random_move == "paper"
+      @outcome = "won"
+    end
     render({ :template => "game_templates/play_scissor"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "zebra", :action => "rules"})
   get("/rock", { :controller => "zebra", :action => "rock"})
   get("/paper", { :controller => "zebra", :action => "paper"})
   get("/scissors", { :controller => "zebra", :action => "scissors"})
